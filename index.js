c60f197b18e7075afa287e236057a7df2cba5f0a
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-01-29 09:23:30 -0600

    Fix name of database module

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 
 //My Modules
-const data = require("./lib/mongodbhelper");
+const data = require("./lib/database");
 
 var app = express();
 var serv = require('http').Server(app);
