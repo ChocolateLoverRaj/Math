60d2ef92a8cbd097a770ebd203385b65ce44afc1
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-10-23 14:17:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var localtunnel = require('localtunnel')
 
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+localtunnel({ port: process.env.PORT, subdomain: 'chocolatemath' }, function (err, tunnel) {
+   console.log(err, tunnel)
 })
 
 serv.listen(process.env.PORT);
